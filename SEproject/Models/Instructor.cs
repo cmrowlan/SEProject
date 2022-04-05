3e2b199e4044b55c47991c1b9524c2691666e451
cucu2000 <cucu_1025@hotmail.com>
2022-04-05 10:48:22 -0500

    Added Classes, Student, and Instructor Models.

---
+++ SEproject/Models/Instructor.cs
@@ -0,0 +1,60 @@
+﻿namespace SEproject.Models;
+
+public class Instructor
+{
+    //Intialize Variables
+    private String instructorID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String position;
+    private String major;
+
+    //GetSets
+    public string GetSetID
+    {
+        get => instructorID;
+        set => instructorID = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string GetSetFirstName
+    {
+        get => firstName;
+        set => firstName = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string GetSetLastName
+    {
+        get => lastName;
+        set => lastName = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string GetSetEmail
+    {
+        get => email;
+        set => email = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string GetSetPosition
+    {
+        get => position;
+        set => position = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string GetSetMajor
+    {
+        get => major;
+        set => major = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    //Constructor
+    public Instructor(string instructorId, string firstName, string lastName, string email, string position, string major)
+    {
+        instructorID = instructorId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.position = position;
+        this.major = major;
+    }
+}
--- SEproject/Models/Student.cs
+++ SEproject/Models/Student.cs
@@ -2,5 +2,52 @@
 {
     public class Student
     {
+        //Intialize Variables
+        private String studentID;
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String major;
+
+        //GetSets
+        public string GetSetID
+        {
+            get => studentID;
+            set => studentID = value ?? throw new ArgumentNullException(nameof(value));
+        }
+        
+        public string GetSetFirstName
+        {
+            get => firstName;
+            set => firstName = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public string GetSetLastName
+        {
+            get => lastName;
+            set => lastName = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public string GetSetEmail
+        {
+            get => email;
+            set => email = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public string GetSetMajor
+        {
+            get => major;
+            set => major = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        //Constructor
+        public Student(string studentID, string firstName, string lastName, string email, string major)
+        {
+            this.studentID = studentID;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.major = major;
+        }
     }
 }
