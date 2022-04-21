0f29a9cd890e8f5ed38d16fca463100aadc07b1a
Benjamin Bowen <bgbowen@my.okcu.edu>
2022-04-20 21:13:09 -0500

    Ability to add new students

--- SEproject/ClientApp/src/app/student-page/student-page.component.ts
+++ SEproject/ClientApp/src/app/student-page/student-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from '../data/models/student';
+import { StudentService } from '../data/student.service';
 
 @Component({
   selector: 'app-student-page',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentPageComponent implements OnInit {
 
-  constructor() { }
+  studentInfo: any = {};
 
-  ngOnInit(): void {
-  }
+  constructor(private studentS: StudentService) { }
+
+    ngOnInit(): void {
+        
+    }
+
+    submit(): void {
+        console.log("studentID: " + this.studentInfo.studentID);
+        console.log("firstName: " + this.studentInfo.firstName);
+        console.log("lastName: " + this.studentInfo.lastName);
+        console.log("email: " + this.studentInfo.email);
+        console.log("major: " + this.studentInfo.major);
+
+        const student = {
+            studentID: this.studentInfo.studentID,
+            firstName: this.studentInfo.firstName,
+            lastName: this.studentInfo.lastName,
+            email: this.studentInfo.email,
+            major: this.studentInfo.major,
+        };
+
+        this.studentS.postStudent(student).subscribe(res => {});
+    }
 
 }
